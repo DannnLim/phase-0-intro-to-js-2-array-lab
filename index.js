075e8e50e27f2741f7ff004ceffb4350265d3f89
DannnLim <Dannylim-@hotmail.com>
2022-06-14 22:23:26 +1000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+}
+
+// console.log(cats.push())
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(Broom) {
+    const appendCatCopy = [...cats, Broom]
+    return appendCatCopy;
+}
+// console.log(appendCat());
+
+function prependCat(Arnold) {
+    const prependCatcopy = [Arnold, ...cats]
+    return prependCatcopy;
+}
+
+// console.log(prependCat());
+
+function removeLastCat() {
+    const appendCatCopy = cats.slice(0, -1);
+    return appendCatCopy;
+}
+
+// console.log(removeLastCat())
+
+function removeFirstCat() {
+    const appendCatCopy = cats.slice(1)
+    return appendCatCopy;
+}
+
+console.log(removeFirstCat())
